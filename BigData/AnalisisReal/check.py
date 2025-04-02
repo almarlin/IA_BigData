aaa5c62b7ab601d153bd35271a1800a1225e744d
almarlin <alvaro.martinez.lineros@gmail.com>
2025-04-02 20:00:17 +0200

    Analisis real - inicio

---
+++ BigData/AnalisisReal/check.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+df = pd.read_csv("muertesINE_stce.csv", delimiter=";")
+
+
+print(f"Columnas: \n{df.columns}\n")
+print(f"Información: \n{df.info()}\n")
+print(f"Primeras entradas:\n{df.head()}\n")
+print(f"Ejemplos: \n{df.sample(5)}")
+
+
+df_totales = df[
+    (df["Sexo"] == "Ambos sexos")
+    | (df["Tamaño de municipio y capital de residencia"] == "Total")
+    | (df["Edad"] == "Todas las edades")
+]
+
+
+
+df = df[~df.index.isin(df_totales.index)]
+
+df["Causas (lista reducida)"] = df["Causas (lista reducida)"].str.strip()
+
+df_ind_totales = df_totales[
+    (df_totales["Sexo"] == "Ambos sexos")
+    & (df_totales["Tamaño de municipio y capital de residencia"] == "Total")
+    & (df_totales["Edad"] == "Todas las edades")
+]
+df_ind_totales.info()
+
+
+df_indices = df[df["Causas (lista reducida)"].str.contains(r"\d{3}-\d{3}", regex=True)]
+# print(df_indices)
+
+df_indices_totales = df_ind_totales[df_ind_totales["Causas (lista reducida)"].str.contains(r"\d{3}-\d{3}", regex=True)]
+df_indices_totales.info()
+# df_indices_totales.info()
+# print(df_indices_totales.head())
+
+# df.to_csv("muertesINE.csv")
